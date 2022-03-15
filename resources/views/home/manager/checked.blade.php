1ac70e4da6922e2de288d99af9220306ea6dcb57
Kidiraliev Alibek <kidiraliev97@inbox.ru>
2022-03-15 10:58:04 +0500

    make auto create manager

--- resources/views/home/manager/checked.blade.php
+++ resources/views/home/manager/checked.blade.php
@@ -6,44 +6,51 @@
             <h2 class="text-center mb-5">Checked messages</h2>
             <hr>
 
-            @if(session('success'))
+            @if (session('success'))
                 <div class="alert alert-success">
-                    {{session('success')}}
+                    {{ session('success') }}
                 </div>
             @endif
 
-            @foreach ($data as $el)
-                <div class="card m-2" style="width: 320px;">
-                    <img src="{{ asset($el->file) }}" style="width: 300px; height: 400px;" class="py-2" alt="">
-                    <div class="card-body">
-                        <h3>Subject: {{ $el->subject }}</h3>
-                        <h5>Message: {{ $el->message }}</h5>
-                        <h5>Name: {{ $el->user->name }}</h5>
-                        <h5>Email: {{ $el->user->email }}</h5>
-                        <p>Created: {{ $el->created_at->format('d F Y. H:i:s') }}</p>
-                        <hr>
 
-                        @if ($el->status === 1)
-                            <form action="{{ route('home.manager.update', ['id' => $el->id]) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="form-group mb-2">
-                                    <textarea class="form-control" name="reply_message"
-                                        placeholder="Reply">{{ $el->feedback->reply_message }}</textarea>
-                                </div>
-                                <button type="submit" class="btn btn-warning">Edit</button>
-                            </form>
-                            <form action="{{ route('home.manager.destroy', ['id' => $el->id]) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Recheck</button>
-                            </form>
-                        @endif
+            @if ($data->all() == null)
+                <h3 class="alert alert-info col-md-4 text-center">
+                    No checked message
+                </h3>
+            @else
+                @foreach ($data as $el)
+                    <div class="card m-2" style="width: 320px;">
+                        <img src="{{ asset($el->file) }}" style="width: 300px; height: 400px;" class="py-2"
+                            alt="">
+                        <div class="card-body">
+                            <h3>Subject: {{ $el->subject }}</h3>
+                            <h5>Message: {{ $el->message }}</h5>
+                            <h5>Name: {{ $el->user->name }}</h5>
+                            <h5>Email: {{ $el->user->email }}</h5>
+                            <p>Created: {{ $el->created_at->format('d F Y. H:i:s') }}</p>
+                            <hr>
 
+                            @if ($el->status === 1)
+                                <form action="{{ route('home.manager.update', ['id' => $el->id]) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="form-group mb-2">
+                                        <textarea class="form-control" name="reply_message"
+                                            placeholder="Reply">{{ $el->feedback->reply_message }}</textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-warning">Edit</button>
+                                </form>
+                                <form action="{{ route('home.manager.destroy', ['id' => $el->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Recheck</button>
+                                </form>
+                            @endif
+
+                        </div>
                     </div>
-                </div>
-            @endforeach
-
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
--- resources/views/home/manager/index.blade.php
+++ resources/views/home/manager/index.blade.php
@@ -22,34 +22,41 @@
                 </div>
             @endif
 
-            @foreach ($data as $el)
-                <div class="card m-2" style="width: 320px;">
-                    <img src="{{ asset($el->file) }}" style="width: 300px; height: 400px;" class="py-2" alt="">
-                    <div class="card-body">
-                        <h3>Subject: {{ $el->subject }}</h3>
-                        <h5>Message: {{ $el->message }}</h5>
-                        <h5>Name: {{ $el->user->name }}</h5>
-                        <h5>Email: {{ $el->user->email }}</h5>
-                        <p>Created: {{ $el->created_at->format('d F Y. H:i:s') }}</p>
-                        <hr>
+            @if ($data->all() == null)
+                <h3 class="alert alert-info col-md-4 text-center">
+                    No new message
+                </h3>
+            @else
+                @foreach ($data as $el)
+                    <div class="card m-2" style="width: 320px;">
+                        <img src="{{ asset($el->file) }}" style="width: 300px; height: 400px;" class="py-2"
+                            alt="">
+                        <div class="card-body">
+                            <h3>Subject: {{ $el->subject }}</h3>
+                            <h5>Message: {{ $el->message }}</h5>
+                            <h5>Name: {{ $el->user->name }}</h5>
+                            <h5>Email: {{ $el->user->email }}</h5>
+                            <p>Created: {{ $el->created_at->format('d F Y. H:i:s') }}</p>
+                            <hr>
 
-                        @if ($el->status === 0)
-                            <form action="{{ route('home.manager.store') }}" method="POST">
-                                @csrf
-                                <div class="form-group mb-2">
-                                    <textarea class="form-control" name="reply_message" placeholder="Reply"></textarea>
+                            @if ($el->status === 0)
+                                <form action="{{ route('home.manager.store') }}" method="POST">
+                                    @csrf
+                                    <div class="form-group mb-2">
+                                        <textarea class="form-control" name="reply_message"
+                                            placeholder="Reply"></textarea>
 
-                                </div>
-                                <input type="hidden" name="user_id" value="{{ $el->user->id }}">
-                                <input type="hidden" name="app_id" value="{{ $el->id }}">
-                                <button type="submit" class="btn btn-primary">Reply</button>
-                            </form>
-                        @endif
+                                    </div>
+                                    <input type="hidden" name="user_id" value="{{ $el->user->id }}">
+                                    <input type="hidden" name="app_id" value="{{ $el->id }}">
+                                    <button type="submit" class="btn btn-primary">Reply</button>
+                                </form>
+                            @endif
 
+                        </div>
                     </div>
-                </div>
-            @endforeach
-
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
